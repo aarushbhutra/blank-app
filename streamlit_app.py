a315349e9e326c7a68e774c331bdfef43a540bbb
Tashi Sherpa Lama <67967009+aarushbhutra@users.noreply.github.com>
2024-09-19 17:50:34 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,19 +1,19 @@
 import streamlit as st
 import pandas as pd
 
-# Load the Excel file with pyarrow as engine
-file_path = 'icl.xlsx'
+# Load the CSV file
+file_path = '/icl.csv'
 
 @st.cache_data
 def load_data():
-    return pd.read_excel(file_path, engine='pyarrow')
+    return pd.read_csv(file_path)
 
 # Load the data
 data = load_data()
 
 # Display the data in Streamlit
 st.title("UG Admissions Statistics (2019-2023)")
-st.write("Displaying data from the uploaded Excel file:")
+st.write("Displaying data from the uploaded CSV file:")
 
 # Show the data in a dataframe
 st.dataframe(data)
